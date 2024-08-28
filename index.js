97c8e33d31496ebc9850e53b54362ef68187fc68
zahrafeisal <zahrafeisal3@gmail.com>
2024-08-28 22:48:30 +0300

    used .filter()

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, driver) {
+    const matching = array.filter(element => element.toLowerCase() === driver.toLowerCase());
+    return matching;
+};
+
+function fuzzyMatch(array, letter) {
+    const matching = array.filter(element => element.startsWith(letter));
+    return matching;
+};
+
+function matchName(array, string) {
+    const matching = array.filter(element => {
+        if (element.name === string) {
+            return element
+        };
+    });
+    return matching;
+};
